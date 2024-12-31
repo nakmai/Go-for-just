261955aabf5dc36e0af833fa973973df50b7f9e5
nakmai <carbani1253@gmail.com>
2024-12-31 16:45:26 +0900

    gitnone

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -28,31 +15,7 @@ class _MyAppState extends State<MyApp> {
       child: MaterialApp(
         title: '目指せジャスト',
         theme: ThemeData.dark(),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Demo'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  key: Key('counter'),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        ),
+        home: HomeScreen(),
       ),
     );
   }
