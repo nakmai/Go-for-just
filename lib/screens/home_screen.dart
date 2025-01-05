6d6b2ee3f9626370029271d5180f0ff0fb208b21
nakmai <carbani1253@gmail.com>
2025-01-05 14:58:31 +0900

    ”秒数強制表示”

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -163,6 +163,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         timerState.stopTimer(_targetSeconds);
                         setState(() {
                           _isTimerStopped = true;
+                          _isTimeVisible = true; // ストップ時に秒数を強制表示
                         });
                       } else {
                         timerState.startTimer();
